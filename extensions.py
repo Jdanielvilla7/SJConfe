63b956ed765fabe092fcfeb6a5f2c8e804795110
Josue Villagran - Vendor <vn595s5@mx.wal-mart.com>
2025-06-14 08:03:28 -0600

    conexión

--- extensions.py
+++ extensions.py
@@ -3,8 +3,17 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "clave_secreta_para_login")
+
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-app.config['SECRET_KEY'] = 'clave_secreta_para_login'
+try:
+    mongo.cx.server_info()  # fuerza la conexión
+    print(" Conexión con MongoDB establecida.")
+except Exception as e:
+    print(f" Error al conectar con MongoDB: {e}")
+
+
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
 mongo = PyMongo(app)
