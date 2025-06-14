161732e1408393aa7649d952e575f1d161089d04
Josue Villagran - Vendor <vn595s5@mx.wal-mart.com>
2025-06-13 23:29:42 -0600

    Captura ex login

--- extensions.py
+++ extensions.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/conferencia_jovenes'
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 app.config['SECRET_KEY'] = 'clave_secreta_para_login'
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
