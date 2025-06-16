8cf66bbea6f29051eb15fb47e1c8ab7f5668513d
Josue Villagran - Vendor <vn595s5@mx.wal-mart.com>
2025-06-15 18:41:50 -0600

    conn

--- extensions.py
+++ extensions.py
@@ -4,11 +4,11 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-# app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "clave_secreta_para_login")
-# app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "clave_secreta_para_login")
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 
-app.config['SECRET_KEY'] = 'tu_clave_secreta'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/conferencia_jovenes'
+# app.config['SECRET_KEY'] = 'tu_clave_secreta'
+# app.config['MONGO_URI'] = 'mongodb://localhost:27017/conferencia_jovenes'
 
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
